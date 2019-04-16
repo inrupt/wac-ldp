3f64e58767e5bbcf24b216d8d2819785e2c3fbe0
Michiel de Jong <michiel@unhosted.org>
2019-04-16 12:38:20 +0200

    sendHttpResponse static function

--- src/lib/operations/ContainerDeleter.ts
+++ src/lib/operations/ContainerDeleter.ts
@@ -1,8 +1,8 @@
 import Debug from 'debug'
 import StorageProcessor from '../../processors/StorageProcessor'
 import Processor from '../../processors/Processor'
-import { LdpResponse, ResultType } from '../api/http/Responder'
-import { LdpTask } from '../api/http/LdpParser'
+import { LdpResponse, ResultType } from '../api/http/HttpResponder'
+import { LdpTask } from '../api/http/HttpParser'
 
 const debug = Debug('ContainerDeleter')
 
