77b597d9ffcb820ca04d8a120658f5ec028b1199
Michiel de Jong <michiel@unhosted.org>
2019-04-16 13:36:46 +0200

    switch to md5 for etags

--- src/lib/util/calculateETag.ts
+++ src/lib/util/calculateETag.ts
@@ -1,10 +1,5 @@
 import * as crypto from 'crypto'
 
-// TODO: make this configurable
-const secret = 'abcdefg'
-
 export function calculateETag (text: string) {
-  const hmac = crypto.createHmac('sha256', secret)
-  hmac.update(text)
-  return hmac.digest('hex')
+  return crypto.createHash('md5').update(text).digest('hex')
 }
