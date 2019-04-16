1881c7826f3250784d0b943083ab177953697ce9
Michiel de Jong <michiel@unhosted.org>
2019-04-16 09:22:21 +0200

    Apply review comments from @jaxoncreed and @RubenVerborgh

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,23 @@ import * as http from 'http'
 import Debug from 'debug'
 
 const debug = Debug('server')
-const port = 8080
+const port = parseInt(process.env.PORT, 10) || 8080
 
-const handler = (req, res) => {
-  res.writeHead(200)
-  res.end('todo: implement')
+class Server {
+  server: http.Server
+  port: number
+  constructor (port: number) {
+    this.server = http.createServer((req, res) => {
+      res.writeHead(200)
+      res.end('todo: implement')
+    })
+  }
+  listen () {
+    this.server.listen(this.port)
+  }
 }
 
-const server = http.createServer(handler)
-
-// ...
-server.listen(port)
+// on startup:
+const server = new Server(port)
+server.listen()
 debug('listening on port', port)
