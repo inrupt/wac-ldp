6594a5920c0d2bc618ffce23a20c4d42958b5595
Michiel de Jong <michiel@unhosted.org>
2019-04-16 10:41:40 +0200

    Paths should start at the root

--- src/lib/storage/BlobTree.ts
+++ src/lib/storage/BlobTree.ts
@@ -1,9 +1,23 @@
 import { Container } from './Container'
 import { Blob } from './Blob'
 
+// The BlobTree is a tree structure. Its internal Nodes are called Containers. Its leaves are called Blobs.
+// A Blob has methods setData and getData, which take and return a ReadableStream, so that you can store opaque
+// data in them.
+// A Container doesn't have much functionality except that you can query a list of its Members (its children in the tree).
+//
+// A path is defined by a list of path segments, which are strings.
+// To allow for convenient slash-separated concatenation of path segments, they are not allowed to contain the '/' character.
+// The Path of a Node is the list of names of nodes you have to visit to get to it.
+// A Path always starts with 'root', and ends with the Node's own name; for instance: ['root', 'foo', 'bar']
+// Sibling Nodes are not allowed to have the same name.
+
 export class Path {
   segments: Array<string>
   constructor (segments: Array<string>) {
+    if (!segments.length || segments[0] !== 'root') {
+      throw new Error('Path should start at the root')
+    }
     segments.map(segment => {
       if (segment.indexOf('/') !== -1) {
         throw new Error('No slashes allowed in path segments!')
--- test/unit/Path_test.ts
+++ test/unit/Path_test.ts
@@ -4,32 +4,38 @@ import { Path } from '../../src/lib/storage/BlobTree'
 
 describe('Path', () => {
   it('removes trailing slashes', function () {
-    const p = new Path(['a', 'b'])
-    expect(p.toString()).to.equal('a/b')
+    const p = new Path(['root', 'a', 'b'])
+    expect(p.toString()).to.equal('root/a/b')
   })
   it('removes leading slashes', function () {
-    const p = new Path(['a', 'b'])
-    expect(p.toString()).to.equal('a/b')
+    const p = new Path(['root', 'a', 'b'])
+    expect(p.toString()).to.equal('root/a/b')
   })
   it('allows for empty segments', function () {
-    const p = new Path(['a', '', 'b'])
-    expect(p.toString()).to.equal('a//b')
+    const p = new Path(['root', 'a', '', 'b'])
+    expect(p.toString()).to.equal('root/a//b')
   })
   it('allows for spaces', function () {
-    const p = new Path(['a ', ' b'])
-    expect(p.toString()).to.equal('a / b')
+    const p = new Path(['root', 'a ', ' b'])
+    expect(p.toString()).to.equal('root/a / b')
   })
   it('allows for newlines', function () {
-    const p = new Path(['a\n', '\rb\t'])
-    expect(p.toString()).to.equal('a\n/\rb\t')
+    const p = new Path(['root', 'a\n', '\rb\t'])
+    expect(p.toString()).to.equal('root/a\n/\rb\t')
   })
   it('allows for dots', function () {
-    const p = new Path(['a', '..', 'b'])
-    expect(p.toString()).to.equal('a/../b')
+    const p = new Path(['root', 'a', '..', 'b'])
+    expect(p.toString()).to.equal('root/a/../b')
   })
   it('does not allow slashes', function () {
     function shouldThrow () {
-      return new Path(['a', '/', 'b'])
+      return new Path(['root', 'a', '/', 'b'])
+    }
+    expect(shouldThrow).to.throw()
+  })
+  it('does not allow relative paths', function () {
+    function shouldThrow () {
+      return new Path(['a', 'b'])
     }
     expect(shouldThrow).to.throw()
   })
