d4e02f81fee4bc9df60371cafe9552147e30d135
Michiel de Jong <michiel@unhosted.org>
2019-04-16 13:38:05 +0200

    switch to base64 for etags

--- src/lib/util/calculateETag.ts
+++ src/lib/util/calculateETag.ts
@@ -1,5 +1,5 @@
 import * as crypto from 'crypto'
 
 export function calculateETag (text: string) {
-  return crypto.createHash('md5').update(text).digest('hex')
+  return crypto.createHash('md5').update(text).digest('base64')
 }
