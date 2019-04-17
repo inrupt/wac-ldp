a377dc9a2c80911bdcf0030929a1712bf6e91306
Michiel de Jong <michiel@unhosted.org>
2019-04-17 16:40:55 +0200

    Rebased #15 on top of #14

--- test/unit/Path.test.ts
+++ test/unit/Path.test.ts
@@ -1,42 +1,40 @@
-import { expect } from 'chai'
-import 'mocha'
 import { Path } from '../../src/lib/storage/BlobTree'
 
 describe('Path', () => {
   it('removes trailing slashes', function () {
     const p = new Path(['root', 'a', 'b'])
-    expect(p.toString()).to.equal('root/a/b')
+    expect(p.toString()).toEqual('root/a/b')
   })
   it('removes leading slashes', function () {
     const p = new Path(['root', 'a', 'b'])
-    expect(p.toString()).to.equal('root/a/b')
+    expect(p.toString()).toEqual('root/a/b')
   })
   it('allows for empty segments', function () {
     const p = new Path(['root', 'a', '', 'b'])
-    expect(p.toString()).to.equal('root/a//b')
+    expect(p.toString()).toEqual('root/a//b')
   })
   it('allows for spaces', function () {
     const p = new Path(['root', 'a ', ' b'])
-    expect(p.toString()).to.equal('root/a / b')
+    expect(p.toString()).toEqual('root/a / b')
   })
   it('allows for newlines', function () {
     const p = new Path(['root', 'a\n', '\rb\t'])
-    expect(p.toString()).to.equal('root/a\n/\rb\t')
+    expect(p.toString()).toEqual('root/a\n/\rb\t')
   })
   it('allows for dots', function () {
     const p = new Path(['root', 'a', '..', 'b'])
-    expect(p.toString()).to.equal('root/a/../b')
+    expect(p.toString()).toEqual('root/a/../b')
   })
   it('does not allow slashes', function () {
     function shouldThrow () {
       return new Path(['root', 'a', '/', 'b'])
     }
-    expect(shouldThrow).to.throw()
+    expect(shouldThrow).toThrow()
   })
   it('does not allow relative paths', function () {
     function shouldThrow () {
       return new Path(['a', 'b'])
     }
-    expect(shouldThrow).to.throw()
+    expect(shouldThrow).toThrow()
   })
 })
