9955f87c290d37cd98a56061bdbb5f31a7159a85
Michiel de Jong <michiel@unhosted.org>
2019-04-17 16:40:55 +0200

    Rebased #15 on top of #14

--- test/unit/BlobTreeInMem.test.ts
+++ test/unit/BlobTreeInMem.test.ts
@@ -1,5 +1,3 @@
-import { expect } from 'chai'
-import 'mocha'
 import { BlobTreeInMem } from '../../src/lib/storage/BlobTreeInMem'
 import { BlobTree, Path } from '../../src/lib/storage/BlobTree'
 import { Blob } from '../../src/lib/storage/Blob'
@@ -16,27 +14,27 @@ describe('BlobTreeInMem', () => {
   it('adds a blob', async function () {
     // non-existing blob
     const blob = this.storage.getBlob(new Path(['root', 'foo']))
-    expect(blob.exists()).to.equal(false)
+    expect(blob.exists()).toEqual(false)
 
     // put data into it
     await blob.setData(Buffer.from('bar'))
-    expect(blob.exists()).to.equal(true)
+    expect(blob.exists()).toEqual(true)
     const readBack2 = await blob.getData()
-    expect(readBack2.toString()).to.equal('bar')
+    expect(readBack2.toString()).toEqual('bar')
   })
 
   it('adds a container', async function () {
     // non-existing container
     const container = this.storage.getContainer(new Path(['root', 'foo']))
-    expect(container.exists()).to.equal(false)
+    expect(container.exists()).toEqual(false)
 
     // add a member
     const blob = this.storage.getBlob(new Path(['root', 'foo', 'bar']))
     await blob.setData(Buffer.from('contents of foo/bar'))
-    expect(container.exists()).to.equal(true)
+    expect(container.exists()).toEqual(true)
 
     const members = await container.getMembers()
-    expect(members).to.deep.equal([
+    expect(members).toEqual([
       { name: 'bar', isContainer: false }
     ])
   })
@@ -52,12 +50,12 @@ describe('BlobTreeInMem', () => {
       const containerFoo: Container = this.storage.getContainer(new Path(['root', 'foo']))
       const containerBaz: Container = this.storage.getContainer(new Path(['root', 'foo', 'baz']))
       const membersFoo = await containerFoo.getMembers()
-      expect(membersFoo).to.deep.equal([
+      expect(membersFoo).toEqual([
         { name: 'bar', isContainer: false },
         { name: 'baz', isContainer: true }
       ])
       const membersBaz = await containerBaz.getMembers()
-      expect(membersBaz).to.deep.equal([
+      expect(membersBaz).toEqual([
         { name: '1', isContainer: false },
         { name: '2', isContainer: false }
       ])
@@ -68,23 +66,23 @@ describe('BlobTreeInMem', () => {
       const blobFooBaz1: Blob = this.storage.getBlob(new Path(['root', 'foo', 'baz', '1']))
 
       // delete foo/bar
-      expect(blobFooBar.exists()).to.equal(true)
+      expect(blobFooBar.exists()).toEqual(true)
       await blobFooBar.delete()
-      expect(blobFooBar.exists()).to.equal(false)
+      expect(blobFooBar.exists()).toEqual(false)
 
       // delete foo/baz/1
-      expect(blobFooBaz1.exists()).to.equal(true)
+      expect(blobFooBaz1.exists()).toEqual(true)
       await blobFooBaz1.delete()
-      expect(blobFooBaz1.exists()).to.equal(false)
+      expect(blobFooBaz1.exists()).toEqual(false)
 
       const containerFoo: Container = this.storage.getContainer(new Path(['root', 'foo']))
       const containerBaz: Container = this.storage.getContainer(new Path(['root', 'foo', 'baz']))
       const membersFoo = await containerFoo.getMembers()
-      expect(membersFoo).to.deep.equal([
+      expect(membersFoo).toEqual([
         { name: 'baz', isContainer: true }
       ])
       const membersBaz = await containerBaz.getMembers()
-      expect(membersBaz).to.deep.equal([
+      expect(membersBaz).toEqual([
         { name: '2', isContainer: false }
       ])
     })
@@ -93,17 +91,17 @@ describe('BlobTreeInMem', () => {
       const containerFooBaz: Container = this.storage.getContainer(new Path(['root', 'foo', 'baz']))
 
       // delete foo/baz/
-      expect(containerFooBaz.exists()).to.equal(true)
+      expect(containerFooBaz.exists()).toEqual(true)
       await containerFooBaz.delete()
-      expect(containerFooBaz.exists()).to.equal(false)
+      expect(containerFooBaz.exists()).toEqual(false)
 
       const containerFoo: Container = this.storage.getContainer(new Path(['root', 'foo']))
       const membersFoo = await containerFoo.getMembers()
-      expect(membersFoo).to.deep.equal([
+      expect(membersFoo).toEqual([
         { name: 'bar', isContainer: false }
       ])
       const membersBaz = await containerFooBaz.getMembers()
-      expect(membersBaz).to.deep.equal([])
+      expect(membersBaz).toEqual([])
     })
   })
 })
