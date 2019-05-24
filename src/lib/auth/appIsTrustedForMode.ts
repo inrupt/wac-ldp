add3d22b6f132c3910608c2443ff005f82fa633f
Michiel de Jong <michiel@unhosted.org>
2019-05-24 15:01:23 +0200

    reading profile quads

--- src/lib/auth/appIsTrustedForMode.ts
+++ src/lib/auth/appIsTrustedForMode.ts
@@ -8,7 +8,7 @@ const debug = Debug('DetermineAllowedModeForOrigin')
 
 const OWNER_PROFILES_FETCH_TIMEOUT = 2000
 
-const ownerProfilesCache: { [webId: string]: string } = {}
+const ownerProfilesCache: { [webId: string]: any } = {}
 
 export interface OriginCheckTask {
   origin: string,
@@ -19,9 +19,22 @@ export interface OriginCheckTask {
 async function checkOwnerProfile (webId: URL, origin: string, mode: string, graphFetcher: RdfFetcher): Promise<boolean> {
   // TODO: move this cache into a decorator pattern, see #81
   if (!ownerProfilesCache[webId.toString()]) {
+    debug('cache miss', webId)
     ownerProfilesCache[webId.toString()] = await graphFetcher.fetchGraph(webId)
   }
-  return Promise.resolve(false)
+  if (!ownerProfilesCache[webId.toString()]) {
+    return Promise.resolve(false)
+  }
+  const quads: Array<any> = []
+  try {
+    ownerProfilesCache[webId.toString()].map((quad: any) => {
+      debug('reading quad', quad)
+      quads.push(quad)
+    })
+  } catch (err) {
+    debug('error looping over quads', err)
+  }
+  return false
 }
 
 export async function appIsTrustedForMode (task: OriginCheckTask, graphFetcher: RdfFetcher): Promise<boolean> {
