749060771fb16a955c121b0c751c69771925d2ee
Michiel de Jong <michiel@unhosted.org>
2019-05-29 11:27:23 +0200

    add TODO in src/lib/rdf/setRootAcl.ts

--- src/lib/rdf/setRootAcl.ts
+++ src/lib/rdf/setRootAcl.ts
@@ -1,8 +1,19 @@
 import { makeResourceData, bufferToStream } from './ResourceDataUtils'
 import { ACL_SUFFIX } from '../rdf/RdfFetcher'
-import { Path, BlobTree } from '../storage/BlobTree'
+import { BlobTree, urlToPath } from '../storage/BlobTree'
 
-export async function setRootAcl (storage: BlobTree, owner: string) {
+// TODO: this should be a method on a CachingRdfLayer object,
+// which has access to a `this.storage` variable rather than
+// requiring that as an extra parameter. And it should also
+// use the CachingRdfLayer object's knowledge of how to map
+// URLs to storage paths
+export async function setRootAcl (storage: BlobTree, owner: string, host: URL) {
+  let hostString = host.toString()
+  if (hostString.substr(-1) !== '/') {
+    hostString += '/'
+  }
+  const rootAclUrl = new URL(hostString + ACL_SUFFIX)
+
   const obj = makeResourceData('text/turtle', [
     `@prefix acl: <http://www.w3.org/ns/auth/acl#>.`,
     `<#owner>`,
@@ -14,6 +25,6 @@ export async function setRootAcl (storage: BlobTree, owner: string) {
     `    acl:Read, acl:Write, acl:Control.`
   ].join('\n'))
   const buffer = Buffer.from(JSON.stringify(obj))
-  const blob = storage.getBlob(new Path(['root', ACL_SUFFIX], false))
+  const blob = storage.getBlob(urlToPath(rootAclUrl))
   await blob.setData(bufferToStream(buffer))
 }
