8a33b70b7bba1eef3e0ba01174664436b2d8ffb4
Michiel de Jong <michiel@unhosted.org>
2019-08-07 09:53:14 +0200

    BufferTreeFs draft

---
+++ src/lib/storage/BufferTreeFs.ts
@@ -0,0 +1,44 @@
+import { Member } from './Container'
+import * as fs from 'fs'
+import * as path from 'path'
+
+export interface ReadableResource {
+  body: ReadableStream<Buffer>
+  contentType: Buffer
+  eTag: Buffer
+}
+
+export interface BufferTree {
+  getMembers (url: URL): Promise<Array<Member> | undefined>
+  getResource (url: URL): Promise<ReadableResource>
+  setResource (url: URL, data: ReadableResource): Promise<void>
+}
+
+function urlToPath (url: URL, contentType: string): string {
+  return url.pathname
+}
+
+function memberPath (folderPath: string, name: string) {
+  return path.join(folderPath, name)
+}
+export class BufferTreeFs implements BufferTree {
+  async getMembers (url: URL): Promise<Array<Member> | undefined> {
+    const folderPath = urlToPath(url)
+    const files: Array<string> = await fs.promises.readdir(folderPath)
+    const results: Array<Member> = []
+    await Promise.all(files.map(async (name): Promise<void> => {
+      const stat = await fs.promises.stat(memberPath(folderPath, name))
+      results.push({
+        name,
+        isContainer: stat.isDirectory()
+      } as Member)
+    }))
+    return results
+  }
+  getResource (url: URL): Promise<ReadableResource> {
+    // try without $.
+    // try with $.
+
+  }
+  setResource (url: URL, data: ReadableResource): Promise<void>
+}
